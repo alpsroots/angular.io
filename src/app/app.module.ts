c29699e201b3f6a1682138d31c2c784109488576
André Ponciano <alpsroots@hotmail.com>
2020-06-23 09:54:00 -0300

    Desenvolvimento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,35 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
-  ],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent,
-    ProductAlertsComponent
-  ],
-  bootstrap: [ AppComponent ]
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      ReactiveFormsModule,
+      RouterModule.forRoot([
+         { path: '', component: ProductListComponent },
+         { path: 'products/:productId', component: ProductDetailsComponent },
+         { path: 'cart', component: CartComponent },
+         { path: 'shipping', component: ShippingComponent },
+       ])
+     ],
+   declarations: [
+      AppComponent,
+      TopBarComponent,
+      ProductListComponent,
+      ProductAlertsComponent,
+      ProductDetailsComponent,
+      CartComponent,
+      ShippingComponent
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
 
@@ -31,4 +44,4 @@ export class AppModule { }
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at http://angular.io/license
-*/
+*/ 
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,15 +1,18 @@
 export const products = [
   {
+    id: 10,
     name: 'Phone XL',
     price: 799,
     description: 'A large phone with one of the best screens'
   },
   {
+    id: 20,
     name: 'Phone Mini',
     price: 699,
     description: 'A great phone with one of the best cameras'
   },
   {
+    id: 30,
     name: 'Phone Standard',
     price: 299,
     description: ''
